edfee3687dd66605a65dea393af46be3a92e088d
Thomas Giles <wthit56@googlemail.com>
2015-10-11 16:24:16 +0100

    Basic markup

--- markup.js
+++ markup.js
@@ -1,9 +1,13 @@
-var markup = function(results /* from inline test */, style) {
+var markup = module.exports = function(results /* from inline test */, styles) {
 	var src = results.src;
-	for (var i = 0, l = results.tests.length; i < l; i++) {
-		var test = results.tests[i];
+	for (var i = 0, test, pass, l = results.tests.length; i < l; i++) {
+		test = results.tests[i];
+		pass = test.passed && !test.failed;
+		
 		src = src.substring(0, test.from) +
-		("[") + src.substring(test.from, test.to) + ("]") +
+		(pass ? styles.passOpen : styles.failOpen) +
+			src.substring(test.from, test.to) +
+		(pass ? styles.passClose : styles.failClose) +
 		src.substring(test.to);
 	}
 	return src;
--- test/markup.js
+++ test/markup.js
@@ -2,12 +2,16 @@ var expect = require("./expect.js"), xexpect = { group: function() {} }, equal =
 
 var inline_test = require("../index.js"), markup = require("../markup.js");
 
+var styles = { passOpen:"[", passClose: "]", failOpen: "<", failClose: ">" };
+
 function test(message, src, expected) {
-	equal("message", markup(inline_test(src)), expected);
+	equal(message, markup(inline_test(src), styles), expected);
 }
 
 test("does not change the source", function() {"code"}, '"code"');
-
+test("does markup passing tests", function() {
+	true /// passes
+}, "\r\n\ttrue [/// passes]\r\n");
 
 /*
 
