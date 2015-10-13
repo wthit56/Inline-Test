e251dd250b9234f977875ea1480b09a7e4a0b839
Thomas Giles <wthit56@googlemail.com>
2015-10-13 15:21:15 +0100

    Main now returns a string

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var parse = /("(?:[^"\r\n]*(?:\\[\W\w])?)*"|'(?:[^'\r\n]*(?:\\[\W\w])?)*')|(;?[ \t]*)\/\/\/[ \t]*[^\r\n]*/g,
-	findBody = /^function\s*[^\s(]*\([^)]*\)\s*\{([\W\w]*)\}$/; 
-var inline_test = module.exports = function(source, callback) {
-	source = source.toString().replace(findBody, "$1");
+	parseFunction = /^(function\s*[^\s(]*\([^)]*\)\s*\{)([\W\w]*)(\})$/,
+	cleanup = /\/\/[^\r\n]*|[\r\n]/g;
+
+var inline_test = module.exports = function(source) {
+	var parsedF = source.toString().match(parseFunction);
+	var body = parsedF[2];
 	var id;
-	while(source.indexOf("result" + (id = Math.random().toString().substr(2))) !== -1);
+	while(body.indexOf("result" + (id = Math.random().toString().substr(2))) !== -1);
 
 	var tests = "", i = 0;
-	require("fs").writeFileSync("test-source.js", source);
-	var rendered = source.replace(parse, function(match, string, pre, index) {
+	require("fs").writeFileSync("test-source.js", body);
+	var rendered = body.replace(parse, function(match, string, pre, index) {
 		if (string) { return match; }
 		else {
 			tests += ", { from:" + (index + (pre ? pre.length : 0)) + ", to:" + (index + match.length) + ", passed: 0, failed: 0 }";
@@ -22,34 +25,29 @@ var inline_test = module.exports = function(source, callback) {
 	});
 
 	var pre = function(result) {
-		var original = process.stdout.write;
-		process.stdout.write = function(data) {
-			//console.log("write");
-			//require("fs").appendFileSync("stdout.txt", data);
-			//stdout.original.write.apply(stdout, arguments);
-			result.stdout += data;
-		};
-		process.stdout.write.original = original;
+		if (typeof process !== "undefined") {
+			var original = process.stdout.write;
+			process.stdout.write = function(data) {
+				result.stdout += data;
+			};
+			process.stdout.write.original = original;
+		}
 	}
 	var post = function(result) {
-		if (callback) { callback(result); }
-		process.stdout.write = process.stdout.write.original;
+		if (typeof process !== "undefined") {
+			process.stdout.write = process.stdout.write.original;
+		}
 		return result;
 	}
 
-	rendered = (
-		"var result" + id + " = { src: " + JSON.stringify(source) + ", stdout: '', tests: [" + tests.substr(1) + "] };" +
-		"(" + pre.toString() + ")(result" + id + ");" +
-		//"\nfunction stderrListener"+id+"(data) { result"+id+".stderr+=data; }"+
-		//"\nprocess.stderr.on('data', stderrListener"+id+").on('error', stderrListener"+id+");" +
-		"\n\n" +
+	rendered = parsedF[1] + (
+		"var result" + id + " = { src: " + JSON.stringify(body) + ", stdout: '', tests: [" + tests.substr(1) + "] };" +
+		"(" + pre.toString().replace(cleanup, "") + ")(result" + id + "); " +
 		rendered +
-		//"\nprocess.stdout.removeListener('data', stdoutListener"+id+");" +
-		//"\nprocess.stderr.removeListener('data', stderrListener"+id+").removeListener('error', stderrListener"+id+");" +
-		"; return (" + post.toString() + ")(result" + id + ");"
-	);
+		"; var result = (" + post.toString().replace(cleanup, "") + ")(result" + id + "); return result;"
+	) + parsedF[3];
 
 	require("fs").writeFileSync("rendered.js", rendered);
 	
-	return new Function("callback,require", rendered);
+	return rendered;
 };
