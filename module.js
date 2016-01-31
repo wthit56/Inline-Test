ac28afc36096355ec065f146e8d72adbedd6f39e
Thomas Giles <wthit56@googlemail.com>
2016-01-31 16:38:30 +0000

    Fixing bugs

--- module.js
+++ module.js
@@ -4,13 +4,15 @@ var fs = 0//require("fs");
 module.exports = function inline_test__module(src, test) {
 	if (!test) { return src.toString(); }
 	else {
+		// console.log("creating it");
+		
 		var it = "";
 		src.toString().replace(parse, function(match, lineTest, multiLineTest) {
 			it += lineTest ? "\n" + lineTest : multiLineTest || "";
 			return match;
 		});
 		
-		fs && fs.appendFileSync("./testRun-output.js", it+"\n\n\n");
-		return 'function() { console.log(require("inline-test/markup")(eval("(" + require("inline-test")(' + new Function(it) + ') + ")()"))); }';
+		fs && fs.appendFileSync("./testRun-output.js", "//inline-test//\n"+it+"\n\n\n");
+		return 'function() { (' + src.toString() + ')(); console.log(require("inline-test/markup")(eval("(" + require("inline-test")(' + new Function(it) + ') + ")()"))); }';
 	}
 };
--- test/module.js
+++ test/module.js
@@ -1,31 +1,27 @@
 var fs = 0//require("fs");
 var module_test = require("../module.js");
 function testRun(test) {
-	var run = false, tested = false;
+	var run = false, tested = false, multitested = false;
 	var newSrc = module_test(function() {
 		"string /// \""
 		'string/// \''
 		// comment / /// test ignored /
 		/* multi-line comment * /// test ignored * */
 		
-		/// a === 1; /// single test
-		/*/
-		a === 1; /// multi test
-		/*/
-		
 		run = true;
 		/// tested = true;
+		/*/ multitested = true; /*/
 	}, test);
-	fs && fs.appendFileSync("./testRun-output.js", newSrc+"\n\n\n");
+	fs && fs.appendFileSync("./testRun-output.js", "//testRun//\n"+newSrc+"\n\n\n");
 	eval("(" + newSrc + ")()");
 	
-	return (test ? (!run && tested) : (run && !tested));
+	return run && (test ? tested && multitested : !tested && !multitested);
 }
 
 var expect = require("./expect.js");
 expect("test = null; should run module", testRun());
 expect("should run module", testRun(false));
-expect("should run tests", testRun(false));
+expect("should run module and tests", testRun(true));
 
 /*
 console.log(require("../markup.js")(eval("(" + require("../index.js")(function() {
