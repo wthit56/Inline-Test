64876e26adc82efa2c4d616fc8525f2e4bda5997
Thomas Giles <wthit56@googlemail.com>
2016-01-31 17:59:14 +0000

    Preserves line and character references

--- module.js
+++ module.js
@@ -1,22 +1,28 @@
 var parse = /\/\/(?=[^\/])[^\r\n]*|\/\*(?=[^\/])(?:[^*]*\*[^\/])*[^*]*\*\/|"(?:[^"\r\n]*\\[\W\w])*[^"\r\n]*"|'(?:[^'\r\n]*\\[\W\w])*[^'\r\n]*'|(\/\/\/[ ]*([^\r\n]*))|(\/\*\/((?:[^\/]*(?:\/\*[^\/]|\/[^*]))*[^\/]*)\/\*\/)/g;
+parse = /([\W\w]*?)(?:(\/\/(?=[^\/])[^\r\n]*|\/\*(?=[^\/])(?:[^*]*\*[^\/])*[^*]*\*\/|"(?:[^"\r\n]*\\[\W\w])*[^"\r\n]*"|'(?:[^'\r\n]*\\[\W\w])*[^'\r\n]*')|(\/\/\/[ ]*([^\r\n]*(?:\r?\n|$)))|(\/\*\/((?:[^\/]*(?:\/\*[^\/]|\/[^*]))*[^\/]*)\/\*\/))/g;
 
-var fs = 0//require("fs");
+var fs = require("fs");
 module.exports = function inline_test__module(src, test) {
 	if (!test) { return src.toString(); }
 	else {
 		// console.log("creating it");
 		
-		var it = "";
-		src.toString().replace(parse, function(match, hasLineTest, lineTest, hasMultiLineTest, multiLineTest) {
+		var it = "", log = "";
+		src.toString().replace(parse, function(match, pre, ignored, hasLineTest, lineTest, hasMultiLineTest, multiLineTest) {
+			ignored = ((pre || "") + (ignored || "")).replace(/\S/g, " ");
+			it += ignored;
 			it += (
-				hasLineTest ? "\n" + lineTest :
-				hasMultiLineTest ? "\n" + multiLineTest :
+				hasLineTest ? "" + lineTest :
+				hasMultiLineTest ? "" + multiLineTest :
 					""
 			);
 			return match;
 		});
 		
-		fs && fs.appendFileSync("./testRun-output.js", "//inline-test//\n"+it+"\n\n\n");
+		console.log("logging");
+		fs && fs.writeFileSync("./inline-test--module--output.js", it);
+		fs && fs.writeFileSync("./inline-test--module--log.js", log);
+		console.log("logged");
 		return 'function() { (' + src.toString() + ')(); console.log(require("inline-test/markup")(eval("(" + require("inline-test")(' + new Function(it) + ') + ")()"))); }';
 	}
 };
